fe36da8d5e95ff5fc34c478c83c9c6bf9431bc1d
Jose Castillo <josegzcasz@gmail.com>
2020-11-26 21:09:25 -0600

    Conexion lista a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 // parse application/x-www-form-urlencoded
@@ -10,7 +11,7 @@ app.use(bodyParser.json())
 
 
 app.get('/', function (req, res) {
-  res.send('<h1> Bienvenido a mi servidor rest </h1>');
+  res.send('<h1> Bienvenido a mi servidor REST (localhost) </h1>');
 });
 
 /*GET para obtener*/
@@ -67,6 +68,15 @@ app.delete('/usuario/:id', function(req, res){
       id: id  
     });
 
+});
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err, res) => {
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => { 
