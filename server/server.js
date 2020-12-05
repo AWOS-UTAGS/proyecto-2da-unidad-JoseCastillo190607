69cb7c4ee20245a2d898ca0678f1b6e64269b966
Jose Castillo <josegzcasz@gmail.com>
2020-12-04 18:50:44 -0600

    Creada la cadena de conexion con mongo atlas en la nube

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+mongoose.connect('mongodb+srv://admin:Emachinesl1@cluster0.p9ut0.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
